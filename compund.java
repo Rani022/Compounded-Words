1d54565f0a41f2a4c1ef03f1f60bd8391f719b38
Rani022 <112650970+Rani022@users.noreply.github.com>
2022-10-01 23:19:17 +0530

    Add files via upload

---
+++ compund.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LongestCompoundWord {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("words for problem.txt");
+		Trie trie = new Trie();
+		LinkedList<Pair<String>> queue = new LinkedList<Pair<String>>();
+		HashSet<String> compoundWords = new HashSet<String>();
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(file);
+
+		String word;			
+		List<Integer> sufIndices;
+		while (s.hasNext()) {
+			word = s.next();		
+			sufIndices = trie.getSuffixesStartIndices(word);
+		
+			for (int i : sufIndices) {
+				if (i >= word.length())		
+					break;
+				queue.add(new Pair<String>(word, word.substring(i)));
+			}
+	
+			trie.insert(word);
+		}
+		
+		Pair<String> p;			
+		int maxLength = 0;			
+		//int sec_maxLength = 0;			
+		String longest = "";		
+		String sec_longest = "";
+
+		while (!queue.isEmpty()) {
+			p = queue.removeFirst();
+			word = p.second();
+			
+			sufIndices = trie.getSuffixesStartIndices(word);
+			if (sufIndices.isEmpty()) {
+				continue;
+			}
+			
+			//System.out.println(word);
+			for (int i : sufIndices) {
+				if (i > word.length()) 
+					break;
+				}
+				
+				if (i == word.length()) { 
+					if (p.first().length() > maxLength) {
+						//sec_maxLength = maxLength;
+						sec_longest = longest;
+						maxLength = p.first().length();
+						longest = p.first();
+					}
+			
+					compoundWords.add(p.first());	
+					
+				} else {
+					queue.add(new Pair<String>(p.first(), word.substring(i)));
+				}
+			}
+		}
+		System.out.println("Longest Compound Word: " + longest);
+		System.out.println("Second Longest Compound Word: " + sec_longest);
+		System.out.println("Total Number of Compound Words: " + compoundWords.size());
+	}
+}
